46312f11c0a8c540d743456746a6ca1ef9a8f49c
Alexander Ose <alexander@parsely.com>
2022-03-04 08:44:31 -0800

    add router

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-function Menu({json}) {
-    if (!json.length) {
-        return <div>Loading...</div>;
-    }
-
-    return (
-        <div>
-            {json.map(d => {
-                return (
-                    <div key={d.name}>
-                        <h3>{d.name}</h3>
-                        <div className="readme" dangerouslySetInnerHTML={{__html: d.readme}}></div>
-                    </div>
-                );
-            })}
-        </div>
-    );
-}
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 
 class App extends React.Component {
     constructor(props) {
@@ -46,12 +29,36 @@ class App extends React.Component {
     render() {
         const {json} = this.state;
         return (
-            <div>
-                <h1>alexosedotdom</h1>
-                <Menu json={json} />
-            </div>
+            <Router>
+                <div>
+                    <h1>alexosedotdom</h1>
+                </div>
+                <Routes>
+                    <Route path="/weather" element={<h2>Weather!</h2>} />
+                    <Route path="/" element={<Menu json={json} />} />
+                </Routes>
+            </Router>
         );
     }
+}
+
+function Menu({json}) {
+    if (!json.length) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <div>
+            {json.map(d => {
+                return (
+                    <div key={d.name}>
+                        <h3>{d.name}</h3>
+                        <div className="readme" dangerouslySetInnerHTML={{__html: d.readme}}></div>
+                    </div>
+                );
+            })}
+        </div>
+    );
 }
 
 ReactDOM.render(<App />, document.getElementById("app"));
